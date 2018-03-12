e59ae0325dbf65c23e19d4f43ce324a96b6967b7
samliebow <sam.liebow@gmail.com>
2018-03-12 13:01:22 -0700

    Edit makeBlinkyDancer to pass tests (inherit prototype, bind 'this')

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,17 +1,23 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = this.step;
-
-  this.step = function() {
+  this.oldStep = this.step;
+  this.step = function step() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    this.oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
     this.$node.toggle();
-  };
+  }.bind(this);
+  this.step();
 };
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype); 
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+//Gives the blinkyDancer all the dancer's shared methods (with line 8) - implied before line 2
+//Runs the makeDancer constructor (line 2)
+//Sets oldStep to the current (dancer) step method (line 4)
+//Defines the new this.step - which we have to do in the constructor,
+//because otherwise it would overwrite this.step before line 4 (in implied before-line-2)
