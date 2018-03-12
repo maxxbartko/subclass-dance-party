9154319bd980b6e53326594c58889b2385b59b8a
samliebow <sam.liebow@gmail.com>
2018-03-12 15:23:51 -0700

    Fix bug in blinkyDancer, make tests pass again

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,17 +1,25 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.timeBetweenSteps = timeBetweenSteps;
+  this.oldStep = makeDancer.prototype.step;
+  this.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    this.oldStep(this.timeBetweenSteps);
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  };
   this.step(this.timeBetweenSteps);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype); 
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinyDancer.prototype.oldStep = makeDancer.prototype.step;
-makeBlinkyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-  this.oldStep(this.timeBetweenSteps);
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
-};
+
+// Having this instead of lines 4-12 above broke in an infinite loop.
+// Not sure why.
+// makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
+// makeBlinkyDancer.prototype.step = function() {
+//   this.oldStep(this.timeBetweenSteps);
+//   this.$node.toggle();
+// };
