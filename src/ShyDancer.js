8d805ad8bff5f050037a23e4ec9f47b21129beac
maxxbartko <maxx.bartko@gmail.com>
2018-03-13 12:18:40 -0700

    Make ShyDancer blush and shrink if others get too close

--- src/ShyDancer.js
+++ src/ShyDancer.js
@@ -16,18 +16,25 @@ ShyDancer.prototype.constructor = ShyDancer;
 
 ShyDancer.prototype.hide = function() {
   var hidden = false;
+  var blushing = false;
   for (var i = 0; i < window.dancers.length; i++) {
     var vertDiff = window.dancers[i].left - this.left;
     var horizDiff = window.dancers[i].top - this.top;
     var hypo = Math.sqrt(vertDiff**2 + horizDiff**2);
-    if (hypo < 100 && hypo !== 0) {
+    if (hypo < 50 && hypo !== 0) {
       hidden = true;
+      this.$node.hide();
+      break;
+    } else if (hypo < 100 && hypo !== 0) {
+      blushing = true;
+      this.$node.css('border', '8px solid red');
     }
   };
-  if (hidden) {
-    this.$node.hide()
-  } else {
+  if (!hidden) {
     this.$node.show();
+  }
+  if (!blushing) {
+    this.$node.css('border', '10px solid blue');
   }
 }
 
